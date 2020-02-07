4b3d75c9ec2c37f026e63d79781eb2d7d40af7bc
Romain Guillot <rguillot42@gmail.com>
2020-02-06 22:49:24 -0500

    Begin test [WIP]

---
+++ app/test/repositories/profile_repository_test.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+
+
+
+
+void main() {
+
+  group("FiresoreProfileRepository", () {
+
+  });
+
+  group("_FirestoreUserAdapter", () {
+
+  });
+}
+
+class MockDocumentReference extends Mock implements DocumentReference {}
+
+class MockFirestore extends Mock implements Firestore {}
+
+class MockCollectionReference extends Mock implements CollectionReference {}
+
+class MockQuerySnapshot extends Mock implements QuerySnapshot {}
+
+class MockDocumentSnapshot extends Mock implements DocumentSnapshot {}
+
+class MockQuery extends Mock implements Query {}
