570d541f0b20b307ebe04e87b14a6361e87005e7
Romain Guillot <rguillot42@gmail.com>
2020-02-08 19:08:57 -0500

    Doc : user model

--- app/lib/models/user.dart
+++ app/lib/models/user.dart
@@ -1,10 +1,20 @@
 // Authors: Romain Guillot and Mamadou Diouldé Diallo
 //
-// Doc: TODO
-// Tests: TODO
+// Doc: Done
+// Tests: Done (not need...)
 
-///
-///
+
+/// Model to represent a user
+/// 
+/// See the documentation `documents > archi_models.md` for more information
+/// 
+/// The class DOES NOT contain any logic inside ! It's not its responsability,
+/// it's responsability it's just a representation of an user.
+/// Some project used the models to convert the model to json ([Map]) of to
+/// convert the json to the model for noSQL database. We decided to NOT do that 
+/// here because these transformations are dependents of the noSQL dataabse
+/// strucutre (name of fields). These transformations are directly applied 
+/// in the corresponding repositories (through adapters)
 class User {
   String name;
   String description;
