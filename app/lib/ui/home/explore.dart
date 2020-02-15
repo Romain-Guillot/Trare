9b11db2a68c883c9caabbeed3ab65dba847418b9
Diouldé Diallo <dioul296@gmail.com>
2020-02-15 01:23:21 +0100

    Amélioration de Explore

--- app/lib/ui/home/explore.dart
+++ app/lib/ui/home/explore.dart
@@ -6,6 +6,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../logic/activity_explore_provider.dart';
+import '../../logic/activity_explore_provider.dart';
+import '../../logic/activity_explore_provider.dart';
+import '../../models/activity.dart';
+
 
 
 class Explore extends StatelessWidget {
@@ -28,23 +33,31 @@ class ListItemsActivities extends StatelessWidget {
     return Consumer<ActivityExploreProvider>(
       builder: (_, activityProvider, __) {
         Widget body; 
-        if (activityProvider.activities == null) {
-          body = Text("Loading");
-        } else {
-          body = ListView.builder(
+        var state=activityProvider.state;
+      switch(state){
+        case ActivityProviderState.loadingInProgress: 
+            body=LoadInprogressWidget(); 
+            break;
+        case ActivityProviderState.databaseError: 
+            body=DatabaseErrorWidget();
+            break;
+        case ActivityProviderState.locationPermissionNotGranted:
+            body=LocationPermissionNotGrantedWidget();
+            break;
+        
+        default: 
+        body=ListView.builder(
             shrinkWrap: true,
             itemCount: activityProvider.activities.length,
             itemBuilder: (_, position) {
               var activity = activityProvider.activities[position];
-              return ListTile(
-                title: Text(activity.title),
-                // subtitle: Text(activity.location),
-                trailing: Icon(Icons.account_circle),
-                onTap: () => openActivity(context, activity),
-              );
+              return ItemActivityWidget(activity);
             } 
           );
-        }
+        break;
+
+      }
+      
         return Column(
           children: <Widget>[
             LocationPermissionRequester(
@@ -70,5 +83,87 @@ class ListItemsActivities extends StatelessWidget {
     Provider.of<ActivityExploreProvider>(context, listen: false).loadActivities();
   }
 }
+
+class LoadInprogressWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+      backgroundColor: Colors.amber,
+      semanticsLabel: Text("Please wait...").toString(),
+      semanticsValue: Text("Please wait...").toString(),  
+    ),
+    );
+  }
+}
+
+class LocationPermissionNotGrantedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text("Sorry locationPermissionNotGranted!!!"),
+    );
+  }
+}
+
+class DatabaseErrorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Sorry DataBaseError!!!"),
+    );
+  }
+}
+
+
+/*class ActivitiesLoadedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    return CircularProgressIndicator(
+      backgroundColor: Colors.amber,
+      semanticsLabel: Text("Please wait...").toString(),
+      semanticsValue: Text("Please wait...").toString(),  
+    );
+     /*ListView.builder(
+            shrinkWrap: true,
+            itemCount: activityProvider.activities.length,
+            itemBuilder: (_, position) {
+              var activity = activityProvider.activities[position];
+              return ListTile(
+                title: Text(activity.title),
+                trailing: Icon(Icons.account_circle),
+                onTap: () => openActivity(context, activity),
+              );
+            } 
+          );*/
+  }
+}*/
+
+class ItemActivityWidget extends StatefulWidget{
+
+Activity _activity;
+  ItemActivityWidget(
+    @required Activity activity
+  ): this._activity=activity;
+
+  @override
+  _ItemActivityWidgetState createState() => _ItemActivityWidgetState();
+}
+
+class _ItemActivityWidgetState extends State<ItemActivityWidget> {
+  @override
+  Widget build(BuildContext context) {
+   
+    return ListTile(
+                title: Text(widget._activity.title),
+                trailing: Icon(Icons.account_circle),
+                onTap: () => {},
+              );;
+  }
+}
+
+
 
 
