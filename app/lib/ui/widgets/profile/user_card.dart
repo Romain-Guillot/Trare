9299088d7cecdb2f0f70e044911522c7470e50bc
Romain Guillot <rguillot42@gmail.com>
2020-02-16 11:05:25 -0500

    Doc: UserCard

--- app/lib/ui/widgets/profile/user_card.dart
+++ app/lib/ui/widgets/profile/user_card.dart
@@ -4,6 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 
+
+/// Widget to display information about an user (name, age, etc)
+///
+/// It takes the [user] as parameter and an optionnal callback [onTap].
+/// 
+/// The layout is a [ListTile] with the following information :
+///   - the user profile picture
+///   - the user name
+///   - the user age
+///   - the user spoken languages
 class UserCard extends StatelessWidget {
 
   final User user;
@@ -22,8 +32,10 @@ class UserCard extends StatelessWidget {
            child: ProfilePicture(url: user.urlPhoto, rounded: true,)
          )
       ),
-      title: Text("${user.name}, ${user.age}"),
-      subtitle: Text(user.spokenLanguages),
+      title: Text(user.name + (user.age != null ? ", ${user.age}" : "")),
+      subtitle: user.spokenLanguages == null || user.spokenLanguages.isEmpty
+                  ? null
+                  : Text(user.spokenLanguages),
       onTap: onTap,
     );
   }
