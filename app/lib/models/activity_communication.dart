cce869144328e93131f35490007faa8feff33e63
Romain Guillot <rguillot42@gmail.com>
2020-03-17 18:32:34 -0400

    Add models ActivityCommunication and Message

---
+++ app/lib/models/activity_communication.dart
@@ -0,0 +1,56 @@
+// Authors: Romain Guillot and Mamadou Diouldé Diallo
+import 'package:app/models/activity.dart';
+import 'package:app/models/user.dart';
+import 'package:flutter/foundation.dart';
+
+
+/// Model to represent the communication system of an activity
+/// 
+/// See the documentation `documents > archi_models.md` for more information
+/// 
+/// The class DOES NOT contain any logic inside ! It's not its responsability,
+/// it's responsability it's just a representation of an activity.
+/// Some project used the models to convert the model to json ([Map]) of to
+/// convert the json to the model for noSQL database. We decided to NOT do that 
+/// here because these transformations are dependents of the noSQL dataabse
+/// strucutre (name of fields). These transformations are directly applied 
+/// in the corresponding services (through adapters)
+class ActivityCommunication {
+  Activity activity;
+  List<User> participants;
+  List<User> interestedUsers;
+  List<Message> messages;
+
+  ActivityCommunication({
+    @required this.activity,
+    this.participants,
+    this.interestedUsers,
+    this.messages,
+  });
+}
+
+
+
+/// Model to represent a message of the [ActivityCommunication] chat
+/// 
+/// See the documentation `documents > archi_models.md` for more information
+/// 
+/// The class DOES NOT contain any logic inside ! It's not its responsability,
+/// it's responsability it's just a representation of an activity.
+/// Some project used the models to convert the model to json ([Map]) of to
+/// convert the json to the model for noSQL database. We decided to NOT do that 
+/// here because these transformations are dependents of the noSQL dataabse
+/// strucutre (name of fields). These transformations are directly applied 
+/// in the corresponding services (through adapters)
+class Message {
+  String id;
+  String content;
+  User user;
+  DateTime publicationDate;
+
+  Message({
+    this.id,
+    @required this.content,
+    @required this.publicationDate,
+  });
+}
